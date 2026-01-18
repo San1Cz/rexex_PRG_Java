528948b81f821c9dae86c0c63773ec937e2a3d1d
matej <petr.matejka@infis.cz>
2026-01-18 11:45:40 +0100

    majerZabava87564

--- src/priklad3.java
+++ src/priklad3.java
@@ -1,5 +1,12 @@
 public class priklad3 {
     public static void main(String[] args) {
-
+        String regex = "[1-9][ABCEHJKLMPSTUZ][0-9]\\s?\\d{4}";
+        String SPZ = "1A1 2345";
+        if (SPZ.matches(regex)) {
+            System.out.println("Je SPZ");
+        }
+        else {
+            System.out.println("Není SPZ");
+        }
     }
 }
