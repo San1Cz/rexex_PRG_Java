da799bacb07bd74ef6cf3745aae6747df6acfa28
matej <petr.matejka@infis.cz>
2026-01-18 11:54:14 +0100

    h

--- src/priklad4.java
+++ src/priklad4.java
@@ -1,5 +1,12 @@
 public class priklad4 {
     public static void main(String[] args) {
-
+        String jmeno = "Petr";
+        String regex = "[A-Z][a-z]{2,}";
+        if (jmeno.matches(regex)) {
+            System.out.println("Je jméno");
+        }
+        else {
+            System.out.println("Není jméno");
+        }
     }
 }
